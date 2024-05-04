83bcffd984b12f9bc4124eba703318a3b026e654
Pikurrot <pikurrot@gmail.com>
2024-05-04 02:31:50 +0200

    Correct to download form 3 versions

--- src/data_downloading.py
+++ src/data_downloading.py
@@ -4,16 +4,17 @@ import os
 import re
 import pandas as pd
 
-def download_image(i, link):
-    subprocess.run(["curl", link, "--output", f"data/images/{i}.jpg"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def download_image(name, link):
+	subprocess.run(["curl", link, "--output", f"data/images/{name}.jpg"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def download_images(df, n):
-    if not os.path.exists("data/images"):
-        os.makedirs("data/images")
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for i in range(n):
-            link = df.iloc[i]["IMAGE_VERSION_1"]
-            executor.submit(download_image, i, link)
+	if not os.path.exists("data/images"):
+		os.makedirs("data/images")
+	with concurrent.futures.ThreadPoolExecutor() as executor:
+		for i in range(n):
+			for j, v in enumerate(("IMAGE_VERSION_1", "IMAGE_VERSION_2", "IMAGE_VERSION_3")):
+				link = df.iloc[i][v]
+				executor.submit(download_image, f"{i}_{j}", link)
             
 # example usage
 # df = pd.read_csv("data/inditextech_hackupc_challenge_images.csv")
